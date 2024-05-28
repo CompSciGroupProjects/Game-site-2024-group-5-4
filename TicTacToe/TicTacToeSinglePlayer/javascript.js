85b71c564766b35f624d8b3e4a1df157aad658e6
PriyumP <126Ppatel@frhsd.com>
2024-05-28 13:22:58 -0400

    adds TTT to group

---
+++ TicTacToe/TicTacToeSinglePlayer/javascript.js
@@ -0,0 +1,214 @@
+function home()
+{
+    window.location.href = "../";
+}
+
+let player = "X";
+function box1()
+{
+
+    if(player === "X")
+    {
+        document.getElementById("box1").innerText = "X";
+    }
+    else
+    {
+        document.getElementById("box1").innerText = "O";
+    }
+
+    if(player === "X")
+    {
+        document.getElementById("player").innerText = "Player O's just placed";
+    }
+    else
+    {
+        document.getElementById("player").innerText = "Player X's just placed";
+    }
+
+    if (player === "X") {
+        player = "O";
+    } else {
+        player = "X";
+    }
+}
+
+function box2()
+{
+
+    if(player === "X")
+    {
+        document.getElementById("box2").innerText = "X";
+    }
+    else
+    {
+        document.getElementById("box2").innerText = "O";
+    }
+
+    if(player === "X")
+    {
+        document.getElementById("player").innerText = "Player O's just placed";
+    }
+    else
+    {
+        document.getElementById("player").innerText = "Player X's just placed";
+    }
+
+    if (player === "X") {
+        player = "O";
+    } else {
+        player = "X";
+    }
+}
+
+function box3()
+{
+
+    if (player === "X") {
+        document.getElementById("box3").innerText = "X";
+    } else {
+        document.getElementById("box3").innerText = "O";
+    }
+
+    if (player === "X") {
+        document.getElementById("player").innerText = "Player O's just placed";
+    } else {
+        document.getElementById("player").innerText = "Player X's just placed";
+    }
+
+    if (player === "X") {
+        player = "O";
+    } else {
+        player = "X";
+    }
+
+}
+
+function box4()
+{
+
+    if (player === "X") {
+        document.getElementById("box4").innerText = "X";
+    } else {
+        document.getElementById("box4").innerText = "O";
+    }
+
+    if (player === "X") {
+        document.getElementById("player").innerText = "Player O's just placed";
+    } else {
+        document.getElementById("player").innerText = "Player X's just placed";
+    }
+
+    if (player === "X") {
+        player = "O";
+    } else {
+        player = "X";
+    }
+
+}
+
+function box5()
+{
+
+    if (player === "X") {
+        document.getElementById("box5").innerText = "X";
+    } else {
+        document.getElementById("box5").innerText = "O";
+    }
+
+    if (player === "X")
+    {
+        document.getElementById("player").innerText = "Player O's just placed";
+    } else
+    {
+        document.getElementById("player").innerText = "Player X's just placed";
+    }
+
+    if (player === "X") {
+        player = "O";
+    } else {
+        player = "X";
+    }
+}
+
+function box6()
+{
+    if (player === "X") {
+        document.getElementById("box6").innerText = "X";
+    } else {
+        document.getElementById("box6").innerText = "O";
+    }
+
+    if (player === "X") {
+        document.getElementById("player").innerText = "Player O's just placed";
+    } else {
+        document.getElementById("player").innerText = "Player X's just placed";
+    }
+
+    if (player === "X") {
+        player = "O";
+    } else {
+        player = "X";
+    }
+}
+
+function box7() {
+    if (player === "X") {
+        document.getElementById("box7").innerText = "X";
+    } else {
+        document.getElementById("box7").innerText = "O";
+    }
+
+    if (player === "X") {
+        document.getElementById("player").innerText = "Player O's just placed";
+    } else {
+        document.getElementById("player").innerText = "Player X's just placed";
+    }
+
+    if (player === "X") {
+        player = "O";
+    } else {
+        player = "X";
+    }
+}
+
+function box8() {
+
+    if (player === "X") {
+        document.getElementById("box8").innerText = "X";
+    } else {
+        document.getElementById("box8").innerText = "O";
+    }
+
+    if (player === "X") {
+        document.getElementById("player").innerText = "Player O's just placed";
+    } else {
+        document.getElementById("player").innerText = "Player X's just placed";
+    }
+
+    if (player === "X") {
+        player = "O";
+    } else {
+        player = "X";
+    }
+}
+
+function box9() {
+
+    if (player === "X") {
+        document.getElementById("box9").innerText = "X";
+    } else {
+        document.getElementById("box9").innerText = "O";
+    }
+
+    if (player === "X") {
+        document.getElementById("player").innerText = "Player O's just placed";
+    } else {
+        document.getElementById("player").innerText = "Player X's just placed";
+    }
+
+    if (player === "X") {
+        player = "O";
+    } else {
+        player = "X";
+    }
+}
---
+++ TicTacToe/javascript.js
@@ -0,0 +1,9 @@
+function singlePlayer()
+{
+    window.location.href = "TicTacToeSinglePlayer";
+}
+
+function multiPlayer()
+{
+    window.location.href = "TicTacToeMultiPlayer";
+}
