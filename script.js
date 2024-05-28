5e7d823c46eeb4c9a3260edc4a259c573b3afc8c
Dan-Dempsey <113116053+Dan-Dempsey@users.noreply.github.com>
2024-05-28 13:55:55 -0400

    some stuff

---
+++ script.js
@@ -0,0 +1,9 @@
+function c4Link() {
+    window.location.href = "Connect4"
+}
+function rPSLink() {
+    window.location.href = "RockPaperScissors"
+}
+function tTTLink() {
+    window.location.href = "TicTacToe"
+}
